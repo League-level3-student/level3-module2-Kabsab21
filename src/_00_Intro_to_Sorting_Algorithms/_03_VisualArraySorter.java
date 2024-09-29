876a480cfccdebb9bcbf867786b67cc24bcedb9c
league <league@192.168.1.214>
2024-09-29 11:30:20 -0700

    AYAHHFUHE :(

--- src/_00_Intro_to_Sorting_Algorithms/_03_VisualArraySorter.java
+++ src/_00_Intro_to_Sorting_Algorithms/_03_VisualArraySorter.java
@@ -1,5 +1,12 @@
 package _00_Intro_to_Sorting_Algorithms;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.sql.Array;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import processing.core.PApplet;
 
 /*
@@ -37,26 +44,55 @@ import processing.core.PApplet;
  * 11. Call the method you made in step 10 when the mouse is pressed using the
  *     mousePressed variable
  */
-public class _03_VisualArraySorter extends PApplet {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 400;
+public class _03_VisualArraySorter extends PApplet implements MouseListener {
+	
+    static final int WIDTH = 500;
+    static final int HEIGHT = 500;
+    int[] ray;
 
     @Override
     public void settings() {
-        
+    setSize(WIDTH, HEIGHT);
+       
+       
     }
 
     @Override
     public void setup() {
-        
+        ray = new int[50];
+        Randomize(ray);
+   
+        noStroke();
     }
 
     @Override
     public void draw() {
-        
+    	if(ray.length == 0) {
+    			setup();
+    	}
+    
+    	background(100, 100, 100);
+    	fill(200, 0, 0);
+    	
+    	   for( int i : ray) {
+             rect(i*(WIDTH/ray.length), HEIGHT, WIDTH/ray.length, -ray[i] );
+    	//	   System.out.println(i);
+         
+           }
+    	         
+           stepSort(ray);
+    }
+    	   
+
+    void Randomize(int[] arr) {
+    	 for( int i : ray) {
+         	ray[i] = (int)random(HEIGHT);
+         }
     }
 
+
     static public void main(String[] passedArgs) {
+
         PApplet.main(_03_VisualArraySorter.class.getName());
     }
     
@@ -77,4 +113,34 @@ public class _03_VisualArraySorter extends PApplet {
       }
       startIndex = 1;
     }
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		Randomize(ray);
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
