db4a178d3e705863d7d892248ba7f211ab1c1d60
league <league@imac-34.attlocal.net>
2024-11-03 11:28:31 -0800

    WAHAHAH

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -8,10 +8,69 @@ import java.util.List;
 public class Algorithms {
     
     public static String multiply(int num1, int num2) {
-        return null;
+    	int ans = num1*num2;
+    	String it = num1+" x "+num2+" = "+ans;
+        return it;
     }
     
     public static int findBrokenEgg(List<String> eggs) {
-        return 0;
+    	
+		for( int i = 2; i < eggs.size(); i++) {
+		
+			if( eggs.get(i) == "cracked") {
+				
+				return i;
+				
+		} 
+			
+		}
+		
+		return 0;
     }
+
+	public static boolean isPrime(int pr) {
+	
+		for( int i = 2; i < pr; i++) {
+		
+			if( pr % i == 0) {
+				
+				return false;
+				
+		} 
+			
+		}
+		
+		return true;
+	}
+	
+
+
+	public static boolean isSquare(int sq) {
+		
+		for( int i = 0; i <= sq; i++) {
+			
+			if( i * i == sq) {
+				
+				return true;
+				
+		} 
+			
+		}
+		
+		return false;
+	}
+
+	public static boolean isCube(int cb) {
+		for( int i = 0; i <= cb; i++) {
+			
+			if( i * i * i == cb) {
+				
+				return true;
+				
+		} 
+			
+		}
+		
+		return false;
+	}
 }
