e912bf0f665494b091720f3adfba20c307b2175b
lvl0_module2 <Eddiejhunter1000000@gmail.com>
2024-11-24 11:28:20 -0800

    AAAAAAAAAAA

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -131,15 +131,18 @@ public class Algorithms {
 		boolean issorted = false;
 		
 		while(issorted == false ){
+			noswap = true;
 			 System.out.println("RUNNING??");
 		int count = 0;	 
-		for(int i = 0; i <  results.size(); i++){
+		for(int i = 0; i <  results.size()-1; i++){
 			
 			 System.out.println("before"+results.get(i)+"size"+results.size());
 			 count++;
-		
+			 
+	if( i < results.size() -1 ) {
 			 
 			if(results.get(i) > results.get(i+1)) {
+				 System.out.println("swap");
 				Double n = results.get(i);
 				 results.set( i, results.get(i+1));
 				 results.set( i+1, n);
@@ -149,16 +152,125 @@ public class Algorithms {
 				 
 				}
 			}
-		if(count == results.size()-2 && noswap == true) {
-			issorted = true;
-			 System.out.println("sorted");
 		}
 		
-	}
+		if(count == results.size()-1 && noswap == true) {
+			 System.out.println("sorted");
+			issorted = true;
+			
+		}
+		
+		}
 		
 	
 		
 		
 		return results;
+	}
+
+	public static Object sortDNA(List<String> un) {
+		// TODO Auto-generated method stub
+		boolean noswap = true;
+		boolean issorted = false;
+		
+		while(issorted == false ){
+			noswap = true;
+			 System.out.println("RUNNING??");
+		int count = 0;	 
+		for(int i = 0; i <  un.size()-1; i++){
+			
+			 System.out.println("before"+un.get(i)+"size"+un.size());
+			 count++;
+			 
+	if( i < un.size() -1 ) {
+			 
+			if(un.get(i).length() > un.get(i+1).length()) {
+				 System.out.println("swap");
+				String n = un.get(i);
+				 un.set( i, un.get(i+1));
+				 un.set( i+1, n);
+				 noswap = false;
+				 System.out.println("swap");
+				
+				 
+				}
+			}
+		}
+		
+		if(count == un.size()-1 && noswap == true) {
+			 System.out.println("sorted");
+			issorted = true;
+			
+		}
+		
+		}
+		return un;
+	}
+
+	public static List<String> sortWords(List<String> words) {
+		// TODO Auto-generated method stub
+		
+		boolean noswap = true;
+		boolean issorted = false;
+		
+		while(issorted == false ){
+			noswap = true;
+			 System.out.println("RUNNING??");
+		int count = 0;	 
+		for(int i = 0; i <  words.size()-1; i++){
+			System.out.println(words.get(i).charAt(1));
+			 System.out.println("before"+words.get(i)+"size"+words.size());
+			 count++;
+			 
+	if( i < words.size() -1 ) {
+			 
+			if(words.get(i).charAt(0) > words.get(i+1).charAt(0)) {
+				 System.out.println("swap");
+				String n = words.get(i);
+				 words.set( i, words.get(i+1));
+				 words.set( i+1, n);
+				 noswap = false;
+				 System.out.println("swap");
+				
+				 
+				} else if(words.get(i).charAt(0) == words.get(i+1).charAt(0)) {
+					 
+					if(words.get(i).charAt(1) > words.get(i+1).charAt(1)) {
+						 System.out.println("swap");
+						String n = words.get(i);
+						 words.set( i, words.get(i+1));
+						 words.set( i+1, n);
+						 noswap = false;
+						 System.out.println("swap");
+						
+						 
+						}
+				}else if(words.get(i).charAt(1) == words.get(i+1).charAt(1)) {
+							 
+							if(words.get(i).charAt(2) > words.get(i+1).charAt(2)) {
+								 System.out.println("swap");
+								String n = words.get(i);
+								 words.set( i, words.get(i+1));
+								 words.set( i+1, n);
+								 noswap = false;
+								 System.out.println("swap");
+								
+								 
+								}
+						}
+				}
+			}
+	 
+		
+		
+		if(count == words.size()-1 && noswap == true) {
+			 System.out.println("sorted");
+			issorted = true;
+			
+		}
+		
+		}	
+
+		return words;
 	}
 }
